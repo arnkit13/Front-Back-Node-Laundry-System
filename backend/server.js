77fec7d5af1676c78e1f9a6aa6d9a19634690408
Anrey Keith Cabigas <cabigasanrey@gmail.com>
2026-08-06 21:09:09 +0800

    chore: enhance CORS error logs

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use(cors({
     if (allowedOrigins.indexOf(origin) !== -1 || allowedOrigins.includes('*')) {
       return callback(null, true);
     }
-    return callback(new Error('Not allowed by CORS'));
+    return callback(new Error(`Not allowed by CORS. Origin: "${origin}", Allowed: ${JSON.stringify(allowedOrigins)}`));
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
